883986e68e7534e7bffe497886a420bbf0bd422d
Simrath Randhawa <srandhawa1250@gmail.com>
2024-07-31 13:22:24 -0500

    first commit

---
+++ scripts/movie-converter.js
@@ -0,0 +1,25 @@
+import { movies } from './movies-data.js';
+
+// if you need how to see this later 
+// const { fishMenu } = require("./restaurant.js")
+// const { mongerInventory } = require("./fishMonger.js");
+
+export const generateMoviesHTML = () => {
+    let movieHTML = '';
+ 
+    for (const movie of movies) {
+        movieHTML += `
+            <article class="movie">
+                <img src="${movie.poster}" alt="${movie.title} poster" class="movie__poster">
+                <div class="movie__details">
+                    <h2 class="movie__title">${movie.title}</h2>
+                    <p class="movie__description">${movie.description}</p>
+                </div>
+            </article>
+        `;
+    }
+ 
+    return movieHTML
+ };
+
+ module.exports = {generateMoviesHTML};
